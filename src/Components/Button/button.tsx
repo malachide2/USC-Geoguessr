8765fe3da0d5cd09cd2abf014037ee9e025f1aad
Johnnnn08 <john17740858850@gmail.com>
2024-11-09 19:25:31 -0800

    Adding homepage

---
+++ src/Components/Button/button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./button.module.css"; 
+
+
+const StartButton = () => {
+  return (
+    <button className="startButton">
+      Start!
+    </button>
+  );
+};
+
+export default StartButton;
