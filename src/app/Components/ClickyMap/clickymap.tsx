e2b03da624846e19731951396e6fbfca286d948f
Max Raffel <44725991+maxraffel@users.noreply.github.com>
2024-11-09 18:22:50 -0800

    clicky map is clicky!

--- src/app/Components/ClickyMap/clickymap.tsx
+++ src/app/Components/ClickyMap/clickymap.tsx
@@ -1,17 +1,24 @@
+'use client'
+
 import Image from "next/image";
 
 export default function ClickyMap() {
-    return (
-      <div>
-        <main>
-          <Image
-            src="/clickymap.png"
-            alt="Map of USC"
-            width={180}
-            height={180}
-            priority
-          />
-        </main>
-      </div>
-    );
+  const clickEvent = (event: React.MouseEvent) => {
+    const { offsetX, offsetY } = event.nativeEvent;
+    console.log(`${offsetX} ${offsetY}`);
+  }
+  return (
+    <div>
+      <main>
+        <Image
+          src="/clickymap.png"
+          alt="Map of USC"
+          width={180}
+          height={180}
+          onClick={clickEvent}
+          priority
+        />
+      </main>
+    </div>
+  );
   }
