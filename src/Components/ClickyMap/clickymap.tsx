9b13e06595bf07e4fc88ac9e4fa78efb17ca27da
Malachi Dewitt <dewitt851@gmail.com>
2024-11-09 22:34:12 -0800

    Overlap Map and Campus Image

--- src/Components/ClickyMap/clickymap.tsx
+++ src/Components/ClickyMap/clickymap.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import React from "react";
 import Image from "next/image";
+import styles from "./clickymap.module.css";
 
 interface Props {
   ClickEvent: (event: React.MouseEvent<HTMLImageElement>) => void;
@@ -8,17 +10,22 @@ interface Props {
 
 const ClickyMap: React.FC<Props> = (props) => {
   return (
-    <div>
-      <main>
-        <Image
-          src="/clickymap.png"
-          alt="Map of USC"
-          width={180}
-          height={180}
-          onClick={props.ClickEvent}
-          priority
-        />
-      </main>
+    <div className={styles.container}>
+      <Image
+          className={styles.image}
+          src="/temp/wall.jpg"
+          alt="Game Image"
+          fill = {true}
+      />
+      <Image
+        className={styles.map}
+        src="/clickymap.png"
+        alt="Map of USC"
+        width={180}
+        height={180}
+        onClick={props.ClickEvent}
+        priority
+      />
     </div>
   );
 }
