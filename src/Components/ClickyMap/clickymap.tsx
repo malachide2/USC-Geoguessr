87b7600bc4d55c4354f479c28e4cf62a664a77a9
Max Raffel <44725991+maxraffel@users.noreply.github.com>
2024-11-10 14:41:28 -0800

    clicky map has been contained!!!!

--- src/Components/ClickyMap/clickymap.tsx
+++ src/Components/ClickyMap/clickymap.tsx
@@ -21,25 +21,35 @@ const ClickyMap: React.FC<Props> = (props) => {
   };
   return (
     <div className={styles.container}>
-      <Image
-          className={styles.image}
-          src={`/temp/${props.ImageTuple[0]}`}
-          alt="Game Image"
-          fill = {true}
-      />
-      <Image
-        className={styles.map}
-        src="/clickymap.png"
-        alt="Map of USC"
-        width={180}
-        height={180}
-        onClick={props.ClickEvent}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        style={{ width: isHovering ? `${props.ZoomedHeight}px` : '15%',  opacity: isHovering ? '1' : '0.75'}}
-        priority
-      />
-    </div>
+        <Image
+            className={styles.image}
+            src={`/temp/${props.ImageTuple[0]}`}
+            alt="Game Image"
+            fill = {true}
+        />
+        <div 
+          className={styles.mapContainer}
+          style={{ width: isHovering ? `${props.ZoomedHeight}px` : '10rem',  opacity: isHovering ? '1' : '0.75', height: isHovering ? `${props.ZoomedHeight}px` : '10rem'}}
+        >
+          <Image
+            className={styles.map}
+            src="/clickymap.png"
+            alt="Map of USC"
+            onClick={props.ClickEvent}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            fill={true}
+            priority
+          />
+          <Image
+            className={styles.marker}
+            src='/marker.png'
+            alt='marker'
+            width={20}
+            height={20}
+          />
+        </div>
+      </div>
   );
 }
 
