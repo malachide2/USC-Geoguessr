f550219c588d0ba2b103ab0eace58bd5d1b4c5d8
Johnnnn08 <john17740858850@gmail.com>
2024-11-10 13:16:17 -0800

    mainpage

--- src/app/Game/page.tsx
+++ src/app/Game/page.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./Game.module.css";
 import Game from "@/Components/Game/game";
-import StartButton from '@/Components/Button/button';
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-      <div /*className='background-image'*/>
-        <h1 className={styles.title}>USC Geoguessr</h1>
-           <StartButton/>
+      <div>
+        <Game/>
       </div>
       </main>
     </div>
   );
 }
-
-
