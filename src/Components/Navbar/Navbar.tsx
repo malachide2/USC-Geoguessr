387db707203dadb4cf0f2f33eda74ff7ca518ec0
Malachi Dewitt <dewitt851@gmail.com>
2024-11-09 18:45:43 -0800

    Updated page layout

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import styles from "./navbar.module.css"
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Blog</div>
+      <div className={styles.logo}>Quit</div>
       <div className={styles.links}></div>
     </div>
   )
